98b3d366cda35550098446724364a98b04820538
Trevor <tdmichaelis@gmail.com>
2025-09-24 12:30:49 -0600

    tm: update image paths

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -19,7 +19,7 @@ export async function GET() {
     {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image:
-        "https://www.totallyrad.dev/images/products/athletic-cotton-socks-6-pairs.jpg",
+        "https://www.totallyrad.dev/product-images/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
       rating: {
         stars: 4.5,
@@ -31,7 +31,7 @@ export async function GET() {
     {
       id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       image:
-        "https://www.totallyrad.dev/images/products/intermediate-composite-basketball.jpg",
+        "https://www.totallyrad.dev/product-images/intermediate-composite-basketball.jpg",
       name: "Intermediate Size Basketball",
       rating: {
         stars: 4,
@@ -43,7 +43,7 @@ export async function GET() {
     {
       id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
       image:
-        "https://www.totallyrad.dev/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+        "https://www.totallyrad.dev/product-images/adults-plain-cotton-tshirt-2-pack-teal.jpg",
       name: "Adults Plain Cotton T-Shirt - 2 Pack",
       rating: {
         stars: 4.5,
@@ -55,7 +55,7 @@ export async function GET() {
     {
       id: "54e0eccd-8f36-462b-b68a-8182611d9add",
       image:
-        "https://www.totallyrad.dev/images/products/black-2-slot-toaster.jpg",
+        "https://www.totallyrad.dev/product-images/black-2-slot-toaster.jpg",
       name: "2 Slot Toaster - Black",
       rating: {
         stars: 5,
@@ -67,7 +67,7 @@ export async function GET() {
     {
       id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
       image:
-        "https://www.totallyrad.dev/images/products/6-piece-white-dinner-plate-set.jpg",
+        "https://www.totallyrad.dev/product-images/6-piece-white-dinner-plate-set.jpg",
       name: "6 Piece White Dinner Plate Set",
       rating: {
         stars: 4,
@@ -79,7 +79,7 @@ export async function GET() {
     {
       id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
       image:
-        "https://www.totallyrad.dev/images/products/6-piece-non-stick-baking-set.webp",
+        "https://www.totallyrad.dev/product-images/6-piece-non-stick-baking-set.webp",
       name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
       rating: {
         stars: 4.5,
@@ -91,7 +91,7 @@ export async function GET() {
     {
       id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
       image:
-        "https://www.totallyrad.dev/images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
+        "https://www.totallyrad.dev/product-images/plain-hooded-fleece-sweatshirt-yellow.jpg",
       name: "Plain Hooded Fleece Sweatshirt",
       rating: {
         stars: 4.5,
@@ -103,7 +103,7 @@ export async function GET() {
     {
       id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
       image:
-        "https://www.totallyrad.dev/images/products/luxury-tower-set-6-piece.jpg",
+        "https://www.totallyrad.dev/product-images/luxury-tower-set-6-piece.jpg",
       name: "Luxury Towel Set - Graphite Gray",
       rating: {
         stars: 4.5,
@@ -115,7 +115,7 @@ export async function GET() {
     {
       id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
       image:
-        "https://www.totallyrad.dev/images/products/liquid-laundry-detergent-plain.jpg",
+        "https://www.totallyrad.dev/product-images/liquid-laundry-detergent-plain.jpg",
       name: "Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz",
       rating: {
         stars: 4.5,
@@ -127,7 +127,7 @@ export async function GET() {
     {
       id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
       image:
-        "https://www.totallyrad.dev/images/products/knit-athletic-sneakers-gray.jpg",
+        "https://www.totallyrad.dev/product-images/knit-athletic-sneakers-gray.jpg",
       name: "Waterproof Knit Athletic Sneakers - Gray",
       rating: {
         stars: 4,
@@ -139,7 +139,7 @@ export async function GET() {
     {
       id: "5968897c-4d27-4872-89f6-5bcb052746d7",
       image:
-        "https://www.totallyrad.dev/images/products/women-chiffon-beachwear-coverup-black.jpg",
+        "https://www.totallyrad.dev/product-images/women-chiffon-beachwear-coverup-black.jpg",
       name: "Women's Chiffon Beachwear Cover Up - Black",
       rating: {
         stars: 4.5,
@@ -151,7 +151,7 @@ export async function GET() {
     {
       id: "aad29d11-ea98-41ee-9285-b916638cac4a",
       image:
-        "https://www.totallyrad.dev/images/products/round-sunglasses-black.jpg",
+        "https://www.totallyrad.dev/product-images/round-sunglasses-black.jpg",
       name: "Round Sunglasses",
       rating: {
         stars: 4.5,
@@ -163,7 +163,7 @@ export async function GET() {
     {
       id: "04701903-bc79-49c6-bc11-1af7e3651358",
       image:
-        "https://www.totallyrad.dev/images/products/women-beach-sandals.jpg",
+        "https://www.totallyrad.dev/product-images/women-beach-sandals.jpg",
       name: "Women's Two Strap Buckle Sandals - Tan",
       rating: {
         stars: 4.5,
@@ -175,7 +175,7 @@ export async function GET() {
     {
       id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
       image:
-        "https://www.totallyrad.dev/images/products/blackout-curtain-set-beige.webp",
+        "https://www.totallyrad.dev/product-images/blackout-curtain-set-beige.webp",
       name: "Blackout Curtains Set 4-Pack - Beige",
       rating: {
         stars: 4.5,
@@ -187,7 +187,7 @@ export async function GET() {
     {
       id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
       image:
-        "https://www.totallyrad.dev/images/products/men-slim-fit-summer-shorts-gray.jpg",
+        "https://www.totallyrad.dev/product-images/men-slim-fit-summer-shorts-gray.jpg",
       name: "Men's Slim-Fit Summer Shorts",
       rating: {
         stars: 4,
@@ -199,7 +199,7 @@ export async function GET() {
     {
       id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
       image:
-        "https://www.totallyrad.dev/images/products/electric-glass-and-steel-hot-water-kettle.webp",
+        "https://www.totallyrad.dev/product-images/electric-glass-and-steel-hot-water-kettle.webp",
       name: "Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter",
       rating: {
         stars: 5,
@@ -211,7 +211,7 @@ export async function GET() {
     {
       id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
       image:
-        "https://www.totallyrad.dev/images/products/facial-tissue-2-ply-18-boxes.jpg",
+        "https://www.totallyrad.dev/product-images/facial-tissue-2-ply-18-boxes.jpg",
       name: "Ultra Soft Tissue 2-Ply - 18 Box",
       rating: {
         stars: 4,
@@ -222,7 +222,7 @@ export async function GET() {
     },
     {
       id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
-      image: "https://www.totallyrad.dev/images/products/straw-sunhat.webp",
+      image: "https://www.totallyrad.dev/product-images/straw-sunhat.webp",
       name: "Straw Lifeguard Sun Hat",
       rating: {
         stars: 4,
@@ -234,7 +234,7 @@ export async function GET() {
     {
       id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
       image:
-        "https://www.totallyrad.dev/images/products/sky-flower-stud-earrings.webp",
+        "https://www.totallyrad.dev/product-images/sky-flower-stud-earrings.webp",
       name: "Sterling Silver Sky Flower Stud Earrings",
       rating: {
         stars: 4.5,
@@ -246,7 +246,7 @@ export async function GET() {
     {
       id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
       image:
-        "https://www.totallyrad.dev/images/products/women-stretch-popover-hoodie-black.jpg",
+        "https://www.totallyrad.dev/product-images/women-stretch-popover-hoodie-black.jpg",
       name: "Women's Stretch Popover Hoodie",
       rating: {
         stars: 4.5,
@@ -257,7 +257,7 @@ export async function GET() {
     },
     {
       id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
-      image: "https://www.totallyrad.dev/images/products/bathroom-rug.jpg",
+      image: "https://www.totallyrad.dev/product-images/bathroom-rug.jpg",
       name: "Bathroom Bath Rug Mat 20 x 31 Inch - Grey",
       rating: {
         stars: 4.5,
@@ -269,7 +269,7 @@ export async function GET() {
     {
       id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
       image:
-        "https://www.totallyrad.dev/images/products/women-knit-ballet-flat-black.jpg",
+        "https://www.totallyrad.dev/product-images/women-knit-ballet-flat-black.jpg",
       name: "Women's Knit Ballet Flat",
       rating: {
         stars: 4,
@@ -281,7 +281,7 @@ export async function GET() {
     {
       id: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
       image:
-        "https://www.totallyrad.dev/images/products/men-golf-polo-t-shirt-blue.jpg",
+        "https://www.totallyrad.dev/product-images/men-golf-polo-t-shirt-blue.jpg",
       name: "Men's Regular-Fit Quick-Dry Golf Polo Shirt",
       rating: {
         stars: 4.5,
@@ -293,7 +293,7 @@ export async function GET() {
     {
       id: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
       image:
-        "https://www.totallyrad.dev/images/products/trash-can-with-foot-pedal-50-liter.jpg",
+        "https://www.totallyrad.dev/product-images/trash-can-with-foot-pedal-50-liter.jpg",
       name: "Trash Can with Foot Pedal - Brushed Stainless Steel",
       rating: {
         stars: 4.5,
@@ -305,7 +305,7 @@ export async function GET() {
     {
       id: "19c6a64a-5463-4d45-9af8-e41140a4100c",
       image:
-        "https://www.totallyrad.dev/images/products/duvet-cover-set-blue-twin.jpg",
+        "https://www.totallyrad.dev/product-images/duvet-cover-set-blue-twin.jpg",
       name: "Duvet Cover Set with Zipper Closure",
       rating: {
         stars: 4,
@@ -317,7 +317,7 @@ export async function GET() {
     {
       id: "d2785924-743d-49b3-8f03-ec258e640503",
       image:
-        "https://www.totallyrad.dev/images/products/women-chunky-beanie-gray.webp",
+        "https://www.totallyrad.dev/product-images/women-chunky-beanie-gray.webp",
       name: "Women's Chunky Cable Beanie - Gray",
       rating: {
         stars: 5,
@@ -336,7 +336,7 @@ export async function GET() {
     {
       id: "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
       image:
-        "https://www.totallyrad.dev/images/products/men-chino-pants-beige.jpg",
+        "https://www.totallyrad.dev/product-images/men-chino-pants-beige.jpg",
       name: "Men's Classic-fit Pleated Chino Pants",
       rating: {
         stars: 4.5,
@@ -348,7 +348,7 @@ export async function GET() {
     {
       id: "1c079479-8586-494f-ab53-219325432536",
       image:
-        "https://www.totallyrad.dev/images/products/men-athletic-shoes-green.jpg",
+        "https://www.totallyrad.dev/product-images/men-athletic-shoes-green.jpg",
       name: "Men's Athletic Sneaker",
       rating: {
         stars: 4,
@@ -360,7 +360,7 @@ export async function GET() {
     {
       id: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
       image:
-        "https://www.totallyrad.dev/images/products/men-navigator-sunglasses-brown.jpg",
+        "https://www.totallyrad.dev/product-images/men-navigator-sunglasses-brown.jpg",
       name: "Men's Navigator Sunglasses Pilot",
       rating: {
         stars: 3.5,
@@ -372,7 +372,7 @@ export async function GET() {
     {
       id: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
       image:
-        "https://www.totallyrad.dev/images/products/non-stick-cooking-set-15-pieces.webp",
+        "https://www.totallyrad.dev/product-images/non-stick-cooking-set-15-pieces.webp",
       name: "Non-Stick Cookware Set, Pots, Pans and Utensils - 15 Pieces",
       rating: {
         stars: 4.5,
@@ -384,7 +384,7 @@ export async function GET() {
     {
       id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
       image:
-        "https://www.totallyrad.dev/images/products/vanity-mirror-silver.jpg",
+        "https://www.totallyrad.dev/product-images/vanity-mirror-silver.jpg",
       name: "Vanity Mirror with Heavy Base - Chrome",
       rating: {
         stars: 4.5,
@@ -396,7 +396,7 @@ export async function GET() {
     {
       id: "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
       image:
-        "https://www.totallyrad.dev/images/products/women-french-terry-fleece-jogger-camo.jpg",
+        "https://www.totallyrad.dev/product-images/women-french-terry-fleece-jogger-camo.jpg",
       name: "Women's Fleece Jogger Sweatpant",
       rating: {
         stars: 4.5,
@@ -408,7 +408,7 @@ export async function GET() {
     {
       id: "d339adf3-e004-4c20-a120-40e8874c66cb",
       image:
-        "https://www.totallyrad.dev/images/products/double-elongated-twist-french-wire-earrings.webp",
+        "https://www.totallyrad.dev/product-images/double-elongated-twist-french-wire-earrings.webp",
       name: "Double Oval Twist French Wire Earrings - Gold",
       rating: {
         stars: 4.5,
@@ -420,7 +420,7 @@ export async function GET() {
     {
       id: "d37a651a-d501-483b-aae6-a9659b0757a0",
       image:
-        "https://www.totallyrad.dev/images/products/round-airtight-food-storage-containers.jpg",
+        "https://www.totallyrad.dev/product-images/round-airtight-food-storage-containers.jpg",
       name: "Round Airtight Food Storage Containers - 5 Piece",
       rating: {
         stars: 4,
@@ -432,7 +432,7 @@ export async function GET() {
     {
       id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
       image:
-        "https://www.totallyrad.dev/images/products/coffeemaker-with-glass-carafe-black.jpg",
+        "https://www.totallyrad.dev/product-images/coffeemaker-with-glass-carafe-black.jpg",
       name: "Coffeemaker with Glass Carafe and Reusable Filter - 25 Oz, Black",
       rating: {
         stars: 4.5,
@@ -444,7 +444,7 @@ export async function GET() {
     {
       id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
       image:
-        "https://www.totallyrad.dev/images/products/blackout-curtains-black.jpg",
+        "https://www.totallyrad.dev/product-images/blackout-curtains-black.jpg",
       name: "Blackout Curtains Set 42 x 84-Inch - Black, 2 Panels",
       rating: {
         stars: 4.5,
@@ -456,7 +456,7 @@ export async function GET() {
     {
       id: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
       image:
-        "https://www.totallyrad.dev/images/products/cotton-bath-towels-teal.webp",
+        "https://www.totallyrad.dev/product-images/cotton-bath-towels-teal.webp",
       name: "100% Cotton Bath Towels - 2 Pack, Light Teal",
       rating: {
         stars: 4.5,
@@ -468,7 +468,7 @@ export async function GET() {
     {
       id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
       image:
-        "https://www.totallyrad.dev/images/products/knit-athletic-sneakers-pink.webp",
+        "https://www.totallyrad.dev/product-images/knit-athletic-sneakers-pink.webp",
       name: "Waterproof Knit Athletic Sneakers - Pink",
       rating: {
         stars: 4,
@@ -480,7 +480,7 @@ export async function GET() {
     {
       id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
       image:
-        "https://www.totallyrad.dev/images/products/countertop-blender-64-oz.jpg",
+        "https://www.totallyrad.dev/product-images/countertop-blender-64-oz.jpg",
       name: "Countertop Blender - 64oz, 1400 Watts",
       rating: {
         stars: 4,
@@ -492,7 +492,7 @@ export async function GET() {
     {
       id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
       image:
-        "https://www.totallyrad.dev/images/products/floral-mixing-bowl-set.jpg",
+        "https://www.totallyrad.dev/product-images/floral-mixing-bowl-set.jpg",
       name: "10-Piece Mixing Bowl Set with Lids - Floral",
       rating: {
         stars: 5,
@@ -504,7 +504,7 @@ export async function GET() {
     {
       id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
       image:
-        "https://www.totallyrad.dev/images/products/kitchen-paper-towels-30-pack.jpg",
+        "https://www.totallyrad.dev/product-images/kitchen-paper-towels-30-pack.jpg",
       name: "2-Ply Kitchen Paper Towels - 30 Pack",
       rating: {
         stars: 4.5,
@@ -516,7 +516,7 @@ export async function GET() {
     {
       id: "bc2847e9-5323-403f-b7cf-57fde044a955",
       image:
-        "https://www.totallyrad.dev/images/products/men-cozy-fleece-zip-up-hoodie-red.jpg",
+        "https://www.totallyrad.dev/product-images/men-cozy-fleece-zip-up-hoodie-red.jpg",
       name: "Men's Full-Zip Hooded Fleece Sweatshirt",
       rating: {
         stars: 4.5,
